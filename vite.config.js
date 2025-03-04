800aae2cb0518d60d2ae70746776e0c1dc7d0a3e
jnlh863 <lopezhernandezjahirnicolas@gmail.com>
2025-03-04 06:25:33 -0600

    fix: Se eliminio estilos bootstrap.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 import { resolve } from 'path'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const path = require('path')
 
@@ -18,10 +15,5 @@ export default {
   server: {
     port: 8080,
 
-  },
-  define: {
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-    'process.env.VITE_API_URL_TEST' : JSON.stringify(process.env.VITE_API_URL_TEST),
-    'process.env.VITE_API_KEY' : JSON.stringify(process.env.VITE_API_KEY),
-  },
+  }
 }
